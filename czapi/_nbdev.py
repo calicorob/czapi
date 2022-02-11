3ae2c81a33169f7db8fe9fe5340f310ed5574777
calicorob <calicorob@users.noreply.github.com>
2022-02-10 19:31:12 -0500

    Add boxscore scraping by gameid

--- czapi/_nbdev.py
+++ czapi/_nbdev.py
@@ -6,13 +6,17 @@ index = {"make_soup": "00_base.ipynb",
          "get_event_name": "01_event.ipynb",
          "get_event_date": "01_event.ipynb",
          "generate_dict_from_table": "02_linescore.ipynb",
+         "get_boxscore_from_table": "02_linescore.ipynb",
+         "get_boxscore_from_game_id": "02_linescore.ipynb",
          "get_table_from_index": "02_linescore.ipynb",
-         "get_boxscore": "02_linescore.ipynb",
-         "__get_boxscore": "02_linescore.ipynb"}
+         "get_boxscore_from_event_and_draw_id": "02_linescore.ipynb",
+         "__get_boxscore": "02_linescore.ipynb",
+         "BOXSCORE_KWARGS": "03_constants.ipynb"}
 
 modules = ["core/scraping/base.py",
            "core/scraping/event.py",
-           "core/scraping/linescore.py"]
+           "core/scraping/linescore.py",
+           "core/scraping/constants.py"]
 
 doc_url = "https://calicorob.github.io/czapi/"
 
