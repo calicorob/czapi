10ab65ac32adcd091be3b49a51bcfa028b4491c5
Robert Currie <robertcurrie@Roberts-MacBook-Pro-2.local>
2023-03-14 18:04:03 -0400

    Create testing.ipynb

--- czapi/_nbdev.py
+++ czapi/_nbdev.py
@@ -3,8 +3,6 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"generate_dict_from_table": "00_base.ipynb",
-         "TagBase": "00_base.ipynb",
-         "TagLike": "00_base.ipynb",
          "normalize_scores": "00_base.ipynb",
          "get_hammer_progressions": "00_base.ipynb",
          "HalfBoxscore": "00_base.ipynb",
@@ -18,12 +16,15 @@ index = {"generate_dict_from_table": "00_base.ipynb",
          "DifferentScoreLengthError": "02_errors.ipynb",
          "InvalidScoreError": "02_errors.ipynb",
          "make_request_from": "03_utils.ipynb",
-         "make_soup_from": "03_utils.ipynb"}
+         "make_soup_from": "03_utils.ipynb",
+         "TagBase": "04_testing.ipynb",
+         "TagLike": "04_testing.ipynb"}
 
 modules = ["core/scraping/base.py",
            "api.py",
            "core/errors.py",
-           "core/utils.py"]
+           "core/utils.py",
+           "core/testing.py"]
 
 doc_url = "https://calicorob.github.io/czapi/"
 
